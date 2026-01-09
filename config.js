1671ec463951b4cc1ba2c07ebc6417dc9cfe31f5
User <user@example.com>
2026-01-09 19:31:10 +0900

    Allow config.js for deployment

---
+++ config.js
@@ -0,0 +1,5 @@
+// Google Apps Script Web App URL
+// このファイルは .gitignore に含まれるため、リポジトリにはコミットされません。
+const CONFIG = {
+    GAS_URL: "https://script.google.com/macros/s/AKfycbwyGBI8izGS4M-yu0U8U4JtLlKyXmOIGywSm7th67tdPHJi_8PbVAsFtxCt4LUtOwu5/exec" // ここにURLを貼り付けてください
+};
